0445b4aafd8cfe4a8a732c7b2ce06446bfbc638f
Kenta Kinjo <e245701@ie.u-ryukyu.ac.jp>
2024-12-03 21:45:22 +0900

    step1

--- app/src/main/java/jp/ac/uryukyu/ie/e245701/Enemy.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e245701/Enemy.java
@@ -8,86 +8,17 @@ package jp.ac.uryukyu.ie.e245701;
  *  boolean dead; //敵の生死状態。true=死亡。
  * Created by tnal on 2016/11/13.
  */
-public class Enemy {
+public class Enemy extends LivingThing{
 
-    /**
-     * 名前(モンスター名)
-     */
-    private String name;
-
-    /**
-     * HP(モンスターのHP)
-     */
-    private int hitPoint;
-
-    /**
-     * 攻撃力(モンスターの攻撃力)
-     */
-    private int attack;
-
-    /**
-     * 死亡判定(trueで死亡)
-     */
-    private boolean dead;
-
-    /**
-     * アクセサ。nameを取得
-     */
-    public String getName() {
-        return name;
+    public Enemy(String name, int hitPoint, int attack) {
+        super(name, hitPoint, attack);
     }
 
-    /**
-     * アクセサ。hitpointを取得
-     */
-    public int getHitpoint() {
-        return hitPoint;
-    }
-
-    /**s
-     * アクセサ。deadを取得
-     */
-    public boolean getDead() {
-        return dead;
-    }
-
-    /**
-     * コンストラクタ。名前、最大HP、攻撃力を指定する。
-     * @param name モンスター名
-     * @param maximumHP モンスターのHP
-     * @param attack モンスターの攻撃力
-     */
-    public Enemy (String name, int maximumHP, int attack) {
-        this.name = name;
-        hitPoint = maximumHP;
-        this.attack = attack;
-        dead = false;
-        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
-    }
-
-    /**
-     * Heroへ攻撃するメソッド。
-     * attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
-     * @param hero 攻撃対象
-     */
-    public void attack(Hero hero){
-        if(hitPoint > 0) {
-            int damage = (int)(Math.random() * attack);
-            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
-            hero.wounded(damage);
-        }
-    }
-
-    /**
-     * 自身へ攻撃されたときのダメージ処理をするメソッド。
-     * 指定されたダメージを hitPoint から引き、死亡判定を行う。
-     * @param damage 受けたダメージ
-     */
-    public void wounded(int damage){
-        hitPoint -= damage;
-        if( hitPoint < 0 ) {
-            dead = true;
-            System.out.printf("モンスター%sは倒れた。\n", name);
+    @Override
+    public void wounded(int damage) {
+        super.wounded(damage);
+        if (isDead()) {
+            System.out.printf("モンスター%sは倒れた。\n", getName());
         }
     }
 }
